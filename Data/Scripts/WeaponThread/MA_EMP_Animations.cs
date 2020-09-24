ea5f64ac3c9552d792f24f30b74aa188c2f4b8e4
AutoMcDonough <dave.m.mcdonough@gmail.com>
2020-09-23 22:47:55 -0400

    Add files via upload

--- Data/Scripts/WeaponThread/MA_EMP_Animations.cs
+++ Data/Scripts/WeaponThread/MA_EMP_Animations.cs
@@ -26,10 +26,57 @@ namespace WeaponThread
 
         private AnimationDef MA_EMP_Animations => new AnimationDef
         {
-			HeatingEmissiveParts = new[]
+		//	HeatingEmissiveParts = new[]
+        //    {
+        //        "Emissive_Bling"
+        //    },
+			Emissives = new[]
             {
-                "Emissive_Bling"
-            },
+             Emissive(
+                    EmissiveName: "EMPHeatUp",
+                    Colors: new []
+                    {
+                        Color(0, 0, 0, 1),
+                        Color(10, .01f, 0, 10),
+                        Color(40, .02f, 0, 60),
+                        Color(60, .04f, 0, 80),
+                        Color(80, .1f, .08f, 120),
+                    },
+                    IntensityFrom:1, //starting intensity, can be 0.0-1.0 or 1.0-0.0, setting both from and to, to the same value will stay at that value
+                    IntensityTo:15,
+                    CycleEmissiveParts: false,//whether to cycle from one part to the next, while also following the Intensity Range, or set all parts at the same time to the same value
+                    LeavePreviousOn: false,//true will leave last part at the last setting until end of animation, used with cycleEmissiveParts
+                    EmissivePartNames: new []
+                    {
+                        "Emissive_Bling"
+                    }
+                ),
+                Emissive(
+                    EmissiveName: "EMPCoolDown",
+                    Colors: new []
+                    {
+                         Color(80, .1f, .08f, 120),      
+                        Color(60, .04f, 0, 80),
+                        Color(40, .02f, 0, 60),
+                        Color(10, .01f, 0, 10),
+                        Color(0, 0, 0, 1),
+						
+						
+						
+						
+                    },
+                    IntensityFrom:1, //starting intensity, can be 0.0-1.0 or 1.0-0.0, setting both from and to, to the same value will stay at that value
+                    IntensityTo:15,
+                    CycleEmissiveParts: false,//whether to cycle from one part to the next, while also following the Intensity Range, or set all parts at the same time to the same value
+                    LeavePreviousOn: false,//true will leave last part at the last setting until end of animation, used with cycleEmissiveParts
+                    EmissivePartNames: new []
+                    {
+                        "Emissive_Bling"
+                    }
+                ),
+			},	
+
+			
             
             EventParticles = new Dictionary<PartAnimationSetDef.EventTriggers, EventParticle[]>
             {
@@ -57,6 +104,55 @@ namespace WeaponThread
                     },
                 },
             },
+			
+			WeaponAnimationSets = new[]
+            {
+			new PartAnimationSetDef()
+                {
+                    SubpartId = Names("GatlingBarrel"),
+                    BarrelId = "Any", //only used for firing, use "Any" for all muzzles
+                    StartupFireDelay = 0,
+                    AnimationDelays = Delays(FiringDelay : 0, ReloadingDelay: 0, OverheatedDelay: 0, TrackingDelay: 0, LockedDelay: 0, OnDelay: 0, OffDelay: 0, BurstReloadDelay: 0, OutOfAmmoDelay: 0, PreFireDelay: 0, StopFiringDelay: 30),//Delay before animation starts
+                    Reverse = Events(),
+                    Loop = Events(),
+                    TriggerOnce = Events(Firing,StopFiring),
+                    EventMoveSets = new Dictionary<PartAnimationSetDef.EventTriggers, RelMove[]>
+                    {
+                        [Firing] =
+                            new[] //Firing, Reloading, Overheated, Tracking, TurnOn, TurnOff, BurstReload, OutOfAmmo, PreFire define a new[] for each
+                            {
+                                new RelMove
+                                {
+                                    CenterEmpty = "",//Specifiy an empty on the subpart to rotate around
+                                    TicksToMove = 190, //number of ticks to complete motion, 60 = 1 second
+
+                                    MovementType = ExpoGrowth, // ExpoGrowth (speedsup),  ExpoDecay (slows down), Linear, Delay, Show, Hide
+                                    EmissiveName = "EMPHeatUp",//name of defined emissive 
+                                    LinearPoints = new XYZ[0],
+                                    Rotation = Transformation(0, 0, 0), //degrees
+                                    RotAroundCenter = Transformation(0, 0, 0), //degrees, rotates around CenterEmpty
+                                },
+                            },
+                        [StopFiring] =
+                            new[] //Firing, Reloading, Overheated, Tracking, TurnOn, TurnOff, BurstReload, OutOfAmmo, PreFire define a new[] for each
+                            {
+
+                                new RelMove
+                                {
+                                    CenterEmpty = "",//Specifiy an empty on the subpart to rotate around
+                                    TicksToMove = 70, //number of ticks to complete motion, 60 = 1 second
+
+                                    MovementType = ExpoDecay, // ExpoGrowth (speedsup),  ExpoDecay (slows down), Linear, Delay, Show, Hide
+                                    EmissiveName = "EMPCoolDown",
+                                    LinearPoints = new XYZ[0],
+                                    Rotation = Transformation(0, 0, 0), //degrees
+                                    RotAroundCenter = Transformation(0, 0, 0), //degrees, rotates around CenterEmpty
+                                },
+
+                            },
+                    }
+                },
+			}
         };
     }
 }
