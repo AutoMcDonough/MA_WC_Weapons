93a34e4cf413cbe12c2a6bd4c148140774ddd607
Mike Dude <62247634+Blaylock1988@users.noreply.github.com>
2020-10-07 15:17:21 -0400

    nerfed gladius EMP to match EMP turret damage

--- Data/Scripts/WeaponThread/MA_Gladius.cs
+++ Data/Scripts/WeaponThread/MA_Gladius.cs
@@ -247,20 +247,20 @@ namespace WeaponThread {
                 Loading = new LoadingDef
                 {
                     RateOfFire = 3600,
-                    BarrelSpinRate = 0, // visual only, 0 disables and uses RateOfFire
+                    BarrelSpinRate = 500, // visual only, 0 disables and uses RateOfFire
                     BarrelsPerShot = 1,
                     TrajectilesPerBarrel = 1, // Number of Trajectiles per barrel per fire event.
                     SkipBarrels = 0,
                     ReloadTime = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
-                    DelayUntilFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
-                    HeatPerShot = 1, //heat generated per shot
-                    MaxHeat = 500, //max heat before weapon enters cooldown (70% of max heat)
-                    Cooldown = .95f, //percent of max heat to be under to start firing again after overheat accepts .2-.95
-                    HeatSinkRate = 30, //amount of heat lost per second
+                    DelayUntilFire = 30, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    HeatPerShot = 15, //10 heat generated per shot
+                    MaxHeat = 1000, //max heat before weapon enters cooldown (70% of max heat)
+                    Cooldown = 0.25f, //percent of max heat to be under to start firing again after overheat accepts .2-.95
+                    HeatSinkRate = 250, //amount of heat lost per second
                     DegradeRof = false, // progressively lower rate of fire after 80% heat threshold (80% of max heat)
-                    ShotsInBurst = 60,
-                    DelayAfterBurst = 120, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
-                    FireFullBurst = true,
+                    ShotsInBurst = 0,
+                    DelayAfterBurst = 300, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    FireFullBurst = false,
                 },
                 Audio = new HardPointAudioDef
                 {
