daba0232418a5c8ac69d1683782c68ea161a2ae6
Mike Dude <62247634+Blaylock1988@users.noreply.github.com>
2020-10-19 23:06:23 -0400

    corrected and simplified barrel animations

--- Data/Scripts/WeaponThread/Blister_Animations.cs
+++ Data/Scripts/WeaponThread/Blister_Animations.cs
@@ -21,7 +21,7 @@ namespace WeaponThread
             {
                 new PartAnimationSetDef()
                 {
-                    SubpartId = Names("barrels3"),
+                    SubpartId = Names("barrels3", "barrels4"),
                     BarrelId = "muzzle_03", //only used for firing, use "Any" for all muzzles
                     StartupFireDelay = 0,
                     AnimationDelays = Delays(FiringDelay : 0, ReloadingDelay: 0, OverheatedDelay: 0, TrackingDelay: 0, LockedDelay: 0, OnDelay: 0, OffDelay: 0, BurstReloadDelay: 0, OutOfAmmoDelay: 0, PreFireDelay: 0, StopFiringDelay: 0),//Delay before animation starts
@@ -55,87 +55,6 @@ namespace WeaponThread
 
                                     MovementType = Linear, // ExpoGrowth (speedsup),  ExpoDecay (slows down), Linear, Delay, Show, Hide
                                     //EmissiveName = "LaserEmissive",//name of defined emissive 
-                                    LinearPoints = new XYZ[0],
-                                    Rotation = Transformation(0, 0, 20f), //degrees
-                                    RotAroundCenter = Transformation(0, 0, 0), //degrees, rotates around CenterEmpty
-                                },
-                            },
-                        [StopFiring] =
-                            new[] //Firing, Reloading, Overheated, Tracking, TurnOn, TurnOff, BurstReload, OutOfAmmo, PreFire define a new[] for each
-                            {
-
-                                new RelMove
-                                {
-                                    CenterEmpty = "",//Specifiy an empty on the subpart to rotate around
-                                    TicksToMove = 15, //number of ticks to complete motion, 60 = 1 second
-
-                                    MovementType = Linear, // ExpoGrowth (speedsup),  ExpoDecay (slows down), Linear, Delay, Show, Hide
-                                    //EmissiveName = "BarrelCoolDown",
-                                    LinearPoints = new XYZ[0],
-                                    Rotation = Transformation(0, 0, 225f), //degrees
-                                    RotAroundCenter = Transformation(0, 0, 0), //degrees, rotates around CenterEmpty
-                                },
-                                new RelMove
-                                {
-                                    CenterEmpty = "",//Specifiy an empty on the subpart to rotate around
-                                    TicksToMove = 15, //number of ticks to complete motion, 60 = 1 second
-
-                                    MovementType = Linear, // ExpoGrowth (speedsup),  ExpoDecay (slows down), Linear, Delay, Show, Hide
-                                    //EmissiveName = "BarrelCoolDown",
-                                    LinearPoints = new XYZ[0],
-                                    Rotation = Transformation(0, 0, 200f), //degrees
-                                    RotAroundCenter = Transformation(0, 0, 0), //degrees, rotates around CenterEmpty
-                                },
-                                new RelMove
-                                {
-                                    CenterEmpty = "",//Specifiy an empty on the subpart to rotate around
-                                    TicksToMove = 15, //number of ticks to complete motion, 60 = 1 second
-
-                                    MovementType = Linear, // ExpoGrowth (speedsup),  ExpoDecay (slows down), Linear, Delay, Show, Hide
-                                    //EmissiveName = "BarrelCoolDown",
-                                    LinearPoints = new XYZ[0],
-                                    Rotation = Transformation(0, 0, 100f), //degrees
-                                    RotAroundCenter = Transformation(0, 0, 0), //degrees, rotates around CenterEmpty
-                                },
-                            },
-                    }
-                },
-                new PartAnimationSetDef()
-                {
-                    SubpartId = Names("barrels4"),
-                    BarrelId = "muzzle_04", //only used for firing, use "Any" for all muzzles
-                    StartupFireDelay = 0,
-                    AnimationDelays = Delays(FiringDelay : 0, ReloadingDelay: 0, OverheatedDelay: 0, TrackingDelay: 0, LockedDelay: 0, OnDelay: 0, OffDelay: 0, BurstReloadDelay: 0, OutOfAmmoDelay: 0, PreFireDelay: 0, StopFiringDelay: 0),//Delay before animation starts
-                    Reverse = Events(),
-                    Loop = Events(Firing),
-                    TriggerOnce = Events(PreFire,Firing,StopFiring),
-                    EventMoveSets = new Dictionary<PartAnimationSetDef.EventTriggers, RelMove[]>
-                    {
-                        [PreFire] =
-                            new[]
-                            {
-                                new RelMove
-                                {
-                                    CenterEmpty = "",//Specifiy an empty on the subpart to rotate around
-                                    TicksToMove = 30, //number of ticks to complete motion, 60 = 1 second
-
-                                    MovementType = ExpoGrowth, // ExpoGrowth (speedsup),  ExpoDecay (slows down), Linear, Delay, Show, Hide
-                                    //EmissiveName = "BarrelHeatUp",//name of defined emissive 
-                                    LinearPoints = new XYZ[0],
-                                    Rotation = Transformation(0, 0, 600f), //degrees
-                                    RotAroundCenter = Transformation(0, 0, 0), //degrees, rotates around CenterEmpty
-                                }
-                            },
-                        [Firing] =
-                            new[] //Firing, Reloading, Overheated, Tracking, TurnOn, TurnOff, BurstReload, OutOfAmmo, PreFire define a new[] for each
-                            {
-                                new RelMove
-                                {
-                                    CenterEmpty = "",//Specifiy an empty on the subpart to rotate around
-                                    TicksToMove = 1, //number of ticks to complete motion, 60 = 1 second
-
-                                    MovementType = Linear, // ExpoGrowth (speedsup),  ExpoDecay (slows down), Linear, Delay, Show, Hide
-                                    //EmissiveName = "BarrelHeatUp",//name of defined emissive 
                                     LinearPoints = new XYZ[0],
                                     Rotation = Transformation(0, 0, 20f), //degrees
                                     RotAroundCenter = Transformation(0, 0, 0), //degrees, rotates around CenterEmpty
