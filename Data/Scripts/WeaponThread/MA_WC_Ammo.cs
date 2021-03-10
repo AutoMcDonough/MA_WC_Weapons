43fd0524a6138866ffcce0cadff859fbdd4a540c
AutoMcDonough <dave.m.mcdonough@gmail.com>
2021-03-10 17:31:44 -0500

    gatling bullet speed increase

--- Data/Scripts/WeaponThread/MA_WC_Ammo.cs
+++ Data/Scripts/WeaponThread/MA_WC_Ammo.cs
@@ -191,7 +191,7 @@ namespace WeaponThread
                 TargetLossTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 MaxLifeTime = 0, // 0 is disabled, Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 AccelPerSec = 0f,
-                DesiredSpeed = 400,
+                DesiredSpeed = 800,
                 MaxTrajectory = 1000f,
                 FieldTime = 0, // 0 is disabled, a value causes the projectile to come to rest, spawn a field and remain for a time (Measured in game ticks, 60 = 1 second)
                 GravityMultiplier = 0f, // Gravity multiplier, influences the trajectory of the projectile, value greater than 0 to enable.
